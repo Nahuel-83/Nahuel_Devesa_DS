87c668b7a78b3ca4444afbfd5b21e8d9e545a143
Nahuel <nahueldevesagil@gmail.com>
2024-09-17 22:13:37 +0200

    Primer nahuel_proyecto

---
+++ demo1/src/test/java/com/nahuel_proyecto/demo1/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nahuel_proyecto.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
