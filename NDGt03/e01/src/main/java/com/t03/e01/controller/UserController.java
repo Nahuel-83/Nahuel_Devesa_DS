90b58fc852f0bb9d141314da4953016cd4e04388
Nahuel <nahueldevesagil@gmail.com>
2024-10-09 21:04:04 +0200

    terminado el ejercicio 3.3, y las practicas tema3

--- NDGt03/e01/src/main/java/com/t03/e01/controller/UserController.java
+++ NDGt03/e01/src/main/java/com/t03/e01/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.t03.e01.controller;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 
 @Controller
@@ -10,12 +16,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UserController {
 
     @GetMapping({"/", "/index", "/home"})
-    public String index() {
-        return "index";
+    public String index(@RequestParam(required = false) String usuario, Model model) {
+    String mensajeBienvenida;
+    
+    if (usuario != null && !usuario.isEmpty()) {
+        mensajeBienvenida = "Bienvenido " + usuario + " a nuestra web";
+    } else {
+        mensajeBienvenida = "Bienvenido a nuestra web";
     }
 
+    model.addAttribute("mensajeBienvenida", mensajeBienvenida);
+    model.addAttribute("year", LocalDate.now().getYear());
+    return "index"; 
+}
+
     @GetMapping("/palmares")
-    public String palmares() {
+    public String palmares(Model model) {
+        List<String> titulos = new ArrayList<>();
+        titulos.add("Liga 2021");
+        titulos.add("Copa del Rey 2020");
+        titulos.add("Supercopa 2019");
+
+        model.addAttribute("titulos", titulos);
         return "palmares";
     }
 
